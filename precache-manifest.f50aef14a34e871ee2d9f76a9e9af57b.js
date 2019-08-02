d07f9394b20d0656e6298ff960f675f0d095f90a
tian <skynapier@outlook.com>
2019-08-02 23:21:27 +1200

    Updates

---
+++ precache-manifest.f50aef14a34e871ee2d9f76a9e9af57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef6dcf81a6d65a0ad939153e58c169f",
+    "url": "/family_tree_app_react/index.html"
+  },
+  {
+    "revision": "957cfdd774bea0029fd7",
+    "url": "/family_tree_app_react/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "5a39a45f7c66e15f7e27",
+    "url": "/family_tree_app_react/static/css/main.4861d4b3.chunk.css"
+  },
+  {
+    "revision": "957cfdd774bea0029fd7",
+    "url": "/family_tree_app_react/static/js/2.c5a34841.chunk.js"
+  },
+  {
+    "revision": "5a39a45f7c66e15f7e27",
+    "url": "/family_tree_app_react/static/js/main.8de46c91.chunk.js"
+  },
+  {
+    "revision": "52853d5061013da6b6fe",
+    "url": "/family_tree_app_react/static/js/runtime~main.64761446.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/family_tree_app_react/precache-manifest.eee473c4d55c38397b264092098091ff.js"
+  "/family_tree_app_react/precache-manifest.f50aef14a34e871ee2d9f76a9e9af57b.js"
 );
 
 self.addEventListener('message', (event) => {
